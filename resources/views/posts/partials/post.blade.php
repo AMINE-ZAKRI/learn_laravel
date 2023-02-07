6660113ccd3734a23048f1b8d2732a385f4dc100
mrtbag <taiwanzr21@gmail.com>
2023-02-07 23:31:23 +0100

    Modify completed commit

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -2,6 +2,6 @@
 <div>{{ $key }}.{{ $post[('title')] }}</div>
 @else 
 <div style="background-color: red">{{ $key }}.{{ $post[('title')] }}</div>
-<div style="background-color: rgb(0, 255, 115)">{{ $key }}.{{ $post[('content')] }}</div>
+<div style="background-color: rgb(223, 203, 23)">{{ $key }}.{{ $post[('content')] }}</div>
 
 @endif
